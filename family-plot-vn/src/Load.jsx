f67187ccd469936c953a22530f9e23f184a373ad
marshmary <phutt2201@gmail.com>
2026-03-22 12:34:58 +0700

    feat: home button reordering & naming

--- family-plot-vn/src/Load.jsx
+++ family-plot-vn/src/Load.jsx
@@ -1,19 +1,18 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import SampleButton from "./SampleButton";
 import faviconSvg from "./img/favicon.svg";
-import ohKayImg from "./img/oh-kay.png";
 import { ThemeToggle } from "./components/common/ThemeToggle";
+import { LanguageSwitcher } from "./components/common/LanguageSwitcher";
+import familyTreeFile from "./gedcoms/family-tree.ged";
 
 const Load = ({
   handleUpload,
   startNewPlot,
-  samples,
   showError,
+  readFile,
 }) => {
   const { t } = useTranslation();
   const [showGedcomModal, setShowGedcomModal] = useState(false);
-  const [showSamples, setShowSamples] = useState(false);
 
   return (
     <>
@@ -30,6 +29,18 @@ const Load = ({
           </section>
 
           <section className="actions-area">
+            {/* Primary CTA - Discover Your Roots */}
+            <div
+              className="action-card action-card-discover"
+              onClick={() => readFile(familyTreeFile)}
+            >
+              <span className="material-icons-outlined action-icon action-icon-discover">
+                auto_graph
+              </span>
+              <h2>{t('home.discoverRoots')}</h2>
+              <p>{t('home.discoverRootsDesc')}</p>
+            </div>
+
             <div className="action-card" onClick={startNewPlot}>
               <span className="material-icons-outlined action-icon">
                 add_circle_outline
@@ -64,51 +75,6 @@ const Load = ({
                 onChange={handleUpload}
               />
             </label>
-
-            <div
-              className="action-card"
-              onClick={() => setShowSamples((prev) => !prev)}
-            >
-              <svg
-                className="action-icon"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 -960 960 960"
-                fill="currentColor"
-              >
-                <path d="M373.5-103.5Q330-147 330-210q0-52 31-91.5t79-53.5v-85H200v-160H100v-280h280v280H280v80h400v-85q-48-14-79-53.5T570-750q0-63 43.5-106.5T720-900q63 0 106.5 43.5T870-750q0 52-31 91.5T760-605v165H520v85q48 14 79 53.5t31 91.5q0 63-43.5 106.5T480-60q-63 0-106.5-43.5Zm396-597Q790-721 790-750t-20.5-49.5Q749-820 720-820t-49.5 20.5Q650-779 650-750t20.5 49.5Q691-680 720-680t49.5-20.5ZM180-680h120v-120H180v120Zm349.5 519.5Q550-181 550-210t-20.5-49.5Q509-280 480-280t-49.5 20.5Q410-239 410-210t20.5 49.5Q451-140 480-140t49.5-20.5ZM240-740Zm480-10ZM480-210Z" />
-              </svg>
-              <h2>{t('load.exploreSamples.title')}</h2>
-              <p>{t('load.exploreSamples.description')}</p>
-            </div>
-
-            {showSamples && (
-              <div className="sample-overlay">
-                <h2 className="sample-overlay-heading">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 -960 960 960"
-                    fill="currentColor"
-                  >
-                    <path d="M373.5-103.5Q330-147 330-210q0-52 31-91.5t79-53.5v-85H200v-160H100v-280h280v280H280v80h400v-85q-48-14-79-53.5T570-750q0-63 43.5-106.5T720-900q63 0 106.5 43.5T870-750q0 52-31 91.5T760-605v165H520v85q48 14 79 53.5t31 91.5q0 63-43.5 106.5T480-60q-63 0-106.5-43.5Zm396-597Q790-721 790-750t-20.5-49.5Q749-820 720-820t-49.5 20.5Q650-779 650-750t20.5 49.5Q691-680 720-680t49.5-20.5ZM180-680h120v-120H180v120Zm349.5 519.5Q550-181 550-210t-20.5-49.5Q509-280 480-280t-49.5 20.5Q410-239 410-210t20.5 49.5Q451-140 480-140t49.5-20.5ZM240-740Zm480-10ZM480-210Z" />
-                  </svg>
-                  {t('load.samplePlots')}
-                </h2>
-                <button
-                  className="sample-overlay-close"
-                  onClick={() => setShowSamples(false)}
-                  aria-label="Close samples"
-                >
-                  <span className="material-icons-outlined">close</span>
-                </button>
-                {samples.map((sample) => (
-                  <SampleButton
-                    key={sample.name}
-                    name={sample.name}
-                    loadFile={sample.load}
-                  />
-                ))}
-              </div>
-            )}
           </section>
 
           {showError && (
@@ -119,14 +85,12 @@ const Load = ({
 
           <section className="links-area">
             <ThemeToggle />
+            <LanguageSwitcher />
             <a
               href="https://github.com/oh-kay-blanket/family-plot"
               aria-label="GitHub"
             >
               <i className="fab fa-github"></i>
-            </a>
-            <a href="https://ohkaycomputer.com" aria-label="Oh, Kay">
-              <img src={ohKayImg} alt="Oh, Kay" className="oh-kay-icon" />
             </a>
           </section>
         </div>
